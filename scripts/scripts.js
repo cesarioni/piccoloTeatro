aa8a8c3c98dc1b46dcaf838e8c12254dd67a7d64
cesarioni <cesarioni@hotmail.com>
2015-09-22 10:51:28 -0500

    carousel update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,17 +3,22 @@ window.onload = function myFunction() {
 	//var loco=document.getElementByClass("imgA");
 	//console.log(document.getElementById("jumboImgCont").style.backgroundColor="red");
 	//document.getElementById("jumboImgCont").style.left="-50px";
-	var posicionCarouselA=document.getElementById("jumboImgCont").style.left;
-	console.log(posicionCarouselA);
-	posicionCarouselA+=10;
-	console.log(posicionCarouselA);
 }
 
 function carouselAction(){
 	var myVar=setInterval(function(){
-			var posicionCarousel=document.getElementById("jumboImgCont").style.left;
-			posicionCarousel+=1;
-			document.getElementById("jumboImgCont").style.left=posicionCarousel;
-		}, 1000);
+			var posicionCarousel=parseInt(getStyle(jumboImgCont, "left"));
+			posicionCarousel+=100;
+			console.log(posicionCarousel);
+			document.getElementById("jumboImgCont").style.left=posicionCarousel+"px";
+			console.log(getStyle(jumboImgCont, "left"));
+		}, 1);
 
+}
+function getStyle(elem, property){
+		if (getComputedStyle !== 'undefined') {
+        	return window.getComputedStyle(elem, null).getPropertyValue(property);
+    	} else {
+        	return elem.currentStyle[property];
+    	}
 }
