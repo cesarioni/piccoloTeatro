3c411b438f056b6b9d60a24ba56ad02021cd0643
cesarioni <cesarioni@hotmail.com>
2015-09-22 10:16:51 -0500

    update carousel

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,19 @@
 // JavaScript Document
 window.onload = function myFunction() {
-	var loco=document.getElementById("lala");
-	loco.style.color="red";
+	//var loco=document.getElementByClass("imgA");
+	//console.log(document.getElementById("jumboImgCont").style.backgroundColor="red");
+	//document.getElementById("jumboImgCont").style.left="-50px";
+	var posicionCarouselA=document.getElementById("jumboImgCont").style.left;
+	console.log(posicionCarouselA);
+	posicionCarouselA+=10;
+	console.log(posicionCarouselA);
+}
+
+function carouselAction(){
+	var myVar=setInterval(function(){
+			var posicionCarousel=document.getElementById("jumboImgCont").style.left;
+			posicionCarousel+=1;
+			document.getElementById("jumboImgCont").style.left=posicionCarousel;
+		}, 1000);
+
 }
