22e9bec61ce9fa24befe031e9ec5c5c059c253bb
cesarioni <cesarioni@hotmail.com>
2015-09-23 10:27:41 -0500

    carousel update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,59 @@
-// JavaScript Document
-window.onload = function myFunction() {
-	//var loco=document.getElementByClass("imgA");
-	//console.log(document.getElementById("jumboImgCont").style.backgroundColor="red");
-	//document.getElementById("jumboImgCont").style.left="-50px";
+
+$(document).ready(function(){
+	var width=960;
+	var animationSpeed=1000;
+	var pause=3000;
+	var currentSlide=1;
+	var transicion=false;
+	//DOM CACHE
+	var $slider=$('#slides');
+	var $slides=$slider.find('.imgSlide');
+	
+	$(".jumboNextArrow").on("click",nextImage);
+	//$(".jumboNextArrow").on("click",nextImage());
+	
+	function createCarouselInterval(){
+		autoCarInterval=setInterval(activateSlider,pause);
+	}
+	
+	function activateSlider(){
+		$slider.animate({'margin-left':'-='+width}, animationSpeed, slideOrder());
+	}
+	
+	function slideOrder(){
+		console.log(currentSlide);
+		if(currentSlide>=$slides.length){
+			currentSlide=1;
+			$slider.css('margin-left', 0);
+			console.log("regreso");
+		}
+		currentSlide++;
+	}
+	//createCarouselInterval();
+
+	function nextImage(){
+		//clearInterval(autoCarInterval);
+		activateSlider();
+	}
+});
+
+
+
+
+
+
+/*window.onload = function myFunction() {
+	
+	
 }
 
 function carouselAction(){
 	var myVar=setInterval(function(){
-			var posicionCarousel=parseInt(getStyle(jumboImgCont, "left"));
-			posicionCarousel+=100;
+			var posicionCarousel=parseInt(getStyle(slides, "margin-left"));
+			posicionCarousel+=10;
 			console.log(posicionCarousel);
-			document.getElementById("jumboImgCont").style.left=posicionCarousel+"px";
-			console.log(getStyle(jumboImgCont, "left"));
+			document.getElementById("slides").style.marginLeft=posicionCarousel+"px";
+			//console.log(getStyle(slides, "left"));
 		}, 1);
 
 }
@@ -21,4 +63,5 @@ function getStyle(elem, property){
     	} else {
         	return elem.currentStyle[property];
     	}
-}
+}*/
+
